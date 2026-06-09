a49cbf0ffe69db258dbb235d9d4d22682e8e185b
lyj348 <3214033664@qq.com>
2026-06-09 17:29:42 +0800

    update src file from pakeplus

---
+++ src/assets/MailTemplateSendForm-BNzUJ2og.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./MailTemplateSendForm.vue_vue_type_script_setup_true_lang-CmzmcUQl.js";import"./index-BVko5nfx.js";import"./Dialog.vue_vue_type_style_index_0_lang-D3thlH7i.js";import"./index-BtKsVXvA.js";export{o as default};
