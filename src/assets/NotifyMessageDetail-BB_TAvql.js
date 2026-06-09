b5014c18d72b8caf5871821c25e6241cb4b263df
lyj348 <3214033664@qq.com>
2026-06-09 17:30:10 +0800

    update src file from pakeplus

---
+++ src/assets/NotifyMessageDetail-BB_TAvql.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./NotifyMessageDetail.vue_vue_type_script_setup_true_lang-BLxM77US.js";import"./Dialog.vue_vue_type_style_index_0_lang-D3thlH7i.js";import"./index-BVko5nfx.js";import"./el-descriptions-item-CLkZ7teT.js";import"./DictTag.vue_vue_type_script_lang-KwIZ3GXR.js";import"./color-DXkOL5Tu.js";import"./formatTime-HgmP7H0U.js";export{o as default};
