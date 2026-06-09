b6e28ec0b38bea499db306f74e021f4ee39be90d
lyj348 <3214033664@qq.com>
2026-06-09 17:32:29 +0800

    update src file from pakeplus

---
+++ src/assets/SocialClientForm--10X5pB6.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./SocialClientForm.vue_vue_type_script_setup_true_lang-D6skeYvr.js";import"./index-BVko5nfx.js";import"./Dialog.vue_vue_type_style_index_0_lang-D3thlH7i.js";export{o as default};
