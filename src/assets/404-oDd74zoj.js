5c876e8a0b51973605a5c451840f8bb925661fbd
lyj348 <3214033664@qq.com>
2026-06-10 10:39:56 +0800

    update src file from pakeplus

---
+++ src/assets/404-oDd74zoj.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./Error.vue_vue_type_script_setup_true_lang-C9Jh3l71.js";import{d as n,o as a,j as e,u,a as m}from"./index-CuCS0KCg.js";const p=n({name:"Error404",__name:"404",setup(c){const{push:s}=m();return(f,r)=>{const o=t;return a(),e(o,{onErrorClick:r[0]||(r[0]=i=>u(s)("/"))})}}});export{p as default};
