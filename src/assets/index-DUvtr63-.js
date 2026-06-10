6539a244fc10561ec5fc63491fd1cc7428b3a2dd
lyj348 <3214033664@qq.com>
2026-06-10 09:30:38 +0800

    update src file from pakeplus

---
+++ src/assets/index-DUvtr63-.js
@@ -0,0 +1,1 @@
+import{b8 as s}from"./index-KvBeQQoQ.js";const a=async t=>await s.get({url:"/exam/test/times-statistics",params:t}),e=async t=>await s.get({url:"/exam/test-result/count-statistics",params:t});export{e as a,a as g};
